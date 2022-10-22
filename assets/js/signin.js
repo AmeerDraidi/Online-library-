9a391ecf9d4e1411c81f70d4ff7d8dad4ad7bc14
shaheerasarama <shaheerasarama@gmail.com>
2022-10-22 16:38:20 +0300

    last

---
+++ assets/js/signin.js
@@ -0,0 +1,21 @@
+localStorage.setItem("admin","admin");
+localStorage.setItem("pass",123456);
+let LocalName = localStorage.getItem("admin");
+let LocalPass = localStorage.getItem("pass");
+let Btn = document.getElementById("loginBtn");
+let name = document.getElementById("name");
+let pass = document.getElementById("pass");
+let alert = document.getElementById("alert");
+let Name = name.value;
+Btn.onclick= function(){
+if(name.value == LocalName && pass.value == LocalPass)
+{
+    location.href = "../../admin.html";
+    sessionStorage.setItem("admin",true);
+}  
+else{
+    alert.classList.replace('d-none','d-block');
+}  
+}
+
+
